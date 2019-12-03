60f470fd5f3e4b641b23f9c989584978deb4f328
Jesse Tippit <jtippit223@gmail.com>
2019-12-03 21:53:52 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,5 @@
+# From the Three Dog Night Song : "Joy to the World" (Jeremiah was a bullfrog)
+puts "Joy to the world"
+puts "all the boys and girls"
+# puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
